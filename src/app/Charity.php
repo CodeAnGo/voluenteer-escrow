028e6a4e2c19ae72054cf74dcdaf73277f326869
Niels Levin <niels.levin@btinternet.com>
2020-04-08 17:06:02 +0100

    Add charity and transfer data models

---
+++ src/app/Charity.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Charity extends Model
+{
+    protected $fillable = [
+        'name',
+        'active'
+    ];
+
+    protected $table = 'charities';
+}
--- src/app/Transfer.php
+++ src/app/Transfer.php
@@ -7,8 +7,25 @@ use SM\Factory\Factory as SMFactory;
 
 class Transfer extends Model
 {
-    protected $attributes = [
-        'status' => TransferStatus::AwaitingAcceptance
+    protected $fillable = [
+        'sending_party_id',
+        'receiving_party_id',
+        'charity_id',
+        'delivery_first_name',
+        'delivery_last_name',
+        'delivery_email',
+        'delivery_country',
+        'delivery_street',
+        'delivery_city',
+        'delivery_town',
+        'delivery_postcode',
+        'charity_id',
+        'transfer_amount',
+        'transfer_reason',
+        'transfer_note',
+        'status' => TransferStatus::AwaitingAcceptance,
+        'stripe_id',
+        'escrow_link',
     ];
 
     public function statusStateMachine()
