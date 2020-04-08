d20e3745daf51918b85955ae9c00eb239b1086d0
jacla1 <jack.clark@netcompany.com>
2020-04-08 13:35:33 +0100

    ESCROW-45 - Register page complete with redirect to Stripe

---
+++ src/resources/views/layouts/base.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>@yield('title')</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    @stack('fonts')
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    @stack('css')
+</head>
+<body>
+    @yield('content')
+</body>
+@stack('js')
+</html>
