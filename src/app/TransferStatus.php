ecba964f86d5ed9139819aa0353111df8f857a1c
Niels Levin <niels.levin@btinternet.com>
2020-04-08 12:30:52 +0100

    Add transfer status state machine

---
+++ src/app/TransferStatus.php
@@ -0,0 +1,14 @@
+<?php
+
+abstract class TransferStatus
+{
+    const AwaitingAcceptance = 1;
+    const Accepted = 2;
+    const Rejected = 3;
+    const Cancelled = 4;
+    const PendingApproval = 5;
+    const Approved = 6;
+    const InDispute = 7;
+    const Closed = 8;
+    const ClosedNonPayment = 9;
+}
