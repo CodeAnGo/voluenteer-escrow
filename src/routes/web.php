ea9a3036a3a7fffeca7c1993f5e46bba71ba67fd
Chris Moran <chmor@netcompany.com>
2020-04-08 15:35:06 +0100

    create frontend - not linked to backend

--- src/routes/web.php
+++ src/routes/web.php
@@ -19,9 +19,7 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('dashboard', function () {
-    return view('dashboard');
-})->name('dashboard')->middleware('auth');
+Route::get('/dashboard', 'TransferController@index')->name('dashboard');
 
 Route::get('/home', 'HomeController@index')->name('home')->middleware('auth');
 
