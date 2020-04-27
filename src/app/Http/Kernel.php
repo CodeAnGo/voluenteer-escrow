a19945ab45524ada7f02410d340b215531acb6df
Luke Roberts <CodeAnGo@users.noreply.github.com>
2020-04-27 12:29:51 +0100

    Refactor Web Routes for Closure Compilation (#57)
    * Refactor Web Routes for Closure Compilation
    
    * Add Stripe Verification Middleware
    
    * Add Striped Route to Web Middleware, Change André Quote from Lorum Ipsum to Actual Quote

--- src/app/Http/Kernel.php
+++ src/app/Http/Kernel.php
@@ -3,6 +3,7 @@
 namespace App\Http;
 
 use App\Http\Middleware\CanViewTransferEvidence;
+use App\Http\Middleware\IsStripeConnected;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
 class Kernel extends HttpKernel
@@ -64,5 +65,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'canViewTransferEvidence' => CanViewTransferEvidence::class,
+        'striped' => IsStripeConnected::class,
     ];
 }
